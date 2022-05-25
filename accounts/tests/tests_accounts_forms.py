e04c6724d34b1072e9c6f798bd988668bf123e3f
Jacek Ostański <jacek@ivolution.pl>
2022-05-25 10:34:58 +0200

    Proper AAA sections

--- accounts/tests/tests_accounts_forms.py
+++ accounts/tests/tests_accounts_forms.py
@@ -3,13 +3,25 @@ from accounts.forms import UserRegisterForm
 
 @pytest.mark.django_db
 def test_user_register_form_valid():
+    # Arrange 
     form = UserRegisterForm({"email": "joe@doe.com", "username":"joe_doe"})
-    assert form.is_valid()
+
+    # Action 
+    result = form.is_valid()
+
+    # Assert
+    assert result == True
 
 @pytest.mark.django_db
 def test_user_register_form_invalid_email():
+    # Arrange
     form = UserRegisterForm({"email": "joe@doe", "username":"joe_doe"})
-    assert not form.is_valid()
+
+    # Action
+    result = form.is_valid()
+
+    # Assert
+    assert result == False
     assert "email" in form.errors
 
 
@@ -17,6 +29,13 @@ def test_user_register_form_invalid_email():
 @pytest.mark.usefixtures("joe_doe_account")
 def test_user_register_form_invalid_duplicated_email():
     # question: is this test resistant to changes?
+    # Arrange
     form = UserRegisterForm({"email": "joe@doe.com", "username":"joe_doe_11"})
-    assert not form.is_valid()
+
+    # Action
+    result = form.is_valid()
+
+    # Assert
+    assert result == False
     assert form.errors["email"] == ["User with this Email address already exists."]
+
