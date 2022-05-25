85a2c6f71f5abb062ce994ac1ae14ee61fa5f7f3
Jacek Ostański <jacek@ivolution.pl>
2022-05-25 13:38:11 +0200

    helper script

--- setup-custom-user-model.sh
+++ setup-custom-user-model.sh
@@ -2,3 +2,5 @@ python manage.py migrate admin zero
 python manage.py migrate auth zero
 python manage.py migrate contenttypes zero
 python manage.py migrate sessions zero
+python manage.py makemigrations accounts
+python manage.py migrate
