ed58cd54791159458b5e780e8b16bc99f0690506
Jacek Ostański <jacek@ivolution.pl>
2022-05-25 13:03:15 +0200

    comment out selenium tests

--- django_testing_examples/tests/tests_initial.py
+++ django_testing_examples/tests/tests_initial.py
@@ -1,28 +1,28 @@
-from django.contrib.staticfiles.testing import StaticLiveServerTestCase
-from selenium import webdriver
-from selenium.webdriver import FirefoxOptions
+# from django.contrib.staticfiles.testing import StaticLiveServerTestCase
+# from selenium import webdriver
+# from selenium.webdriver import FirefoxOptions
 
 
-class InitialTestCase(StaticLiveServerTestCase):
-    """
-    Example smoke test to check if framework has started, using Selenium.
-    Is this test valuable?
-    - It only starts a page and checks if it runs. 
-    - fails to verify incorrect db setup, like wrong name or password
-    - takes 4 seconds to run
-    Curious case to show, but not much more. Extreme TDD.
-    """
-    def setUp(self):
-        super().setUp()
-        opts = FirefoxOptions()
-        opts.add_argument("--headless")
-        self.selenium = webdriver.Firefox(options=opts)
+# class InitialTestCase(StaticLiveServerTestCase):
+#     """
+#     Example smoke test to check if framework has started, using Selenium.
+#     Is this test valuable?
+#     - It only starts a page and checks if it runs. 
+#     - fails to verify incorrect db setup, like wrong name or password
+#     - takes 4 seconds to run
+#     Curious case to show, but not much more. Extreme TDD.
+#     """
+#     def setUp(self):
+#         super().setUp()
+#         opts = FirefoxOptions()
+#         opts.add_argument("--headless")
+#         self.selenium = webdriver.Firefox(options=opts)
 
-    def tearDown(self):
-        self.selenium.quit()
-        super().tearDown()
+#     def tearDown(self):
+#         self.selenium.quit()
+#         super().tearDown()
 
-    def test_smoke_server_started(self):
-        self.selenium.get('%s%s' % (self.live_server_url, '/admin/'))
-        assert 'Django' in self.selenium.title
+#     def test_smoke_server_started(self):
+#         self.selenium.get('%s%s' % (self.live_server_url, '/admin/'))
+#         assert 'Django' in self.selenium.title
 
--- django_testing_examples/tests/tests_initial_pytest.py
+++ django_testing_examples/tests/tests_initial_pytest.py
@@ -1,13 +1,13 @@
 
-import pytest
+# import pytest
 
-def test_pytest_live_server_no_mark(live_server, selenium):
-    # Arrange
-    # in fixture
+# def test_pytest_live_server_no_mark(live_server, selenium):
+#     # Arrange
+#     # in fixture
 
-    #Action
-    selenium.get('%s%s' % (live_server.url, '/admin/'))
+#     #Action
+#     selenium.get('%s%s' % (live_server.url, '/admin/'))
 
-    # Assert
-    assert 'Django' in selenium.title
+#     # Assert
+#     assert 'Django' in selenium.title
 
