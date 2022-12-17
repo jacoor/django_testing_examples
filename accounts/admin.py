6ce6decf36a41a19bfc4ff95261bc2d7418fc96c
Jacek Ostański <jacek@ivolution.pl>
2022-12-17 17:36:02 +0100

    student models

--- accounts/admin.py
+++ accounts/admin.py
@@ -2,6 +2,8 @@ from django.contrib import admin
 
 # Register your models here.
 
-from .models import ContactMessage
+from .models import ContactMessage,  StudentCard, Student
 
 admin.site.register(ContactMessage)
+admin.site.register(Student)
+admin.site.register(StudentCard)
--- accounts/models.py
+++ accounts/models.py
@@ -2,6 +2,29 @@ from django.db import models
 from django.contrib.auth.models import AbstractUser
 # Create your models here.
 
+class Student(models.Model):
+    first_name = models.CharField(max_length=255)
+    middle_name = models.CharField(max_length=255, blank=True)
+    last_name = models.CharField(max_length=255)
+    email = models.EmailField(max_length=255)
+    birth_date = models.DateField()
+    phone_number = models.CharField(max_length=255)
+    addres_street = models.CharField(max_length=255)
+    address_city = models.CharField(max_length=255)
+    address_zipcode = models.CharField(max_length=255)
+
+    def __str__(self):
+        return f"{self.first_name} {self.last_name}"
+
+class StudentCard(models.Model):
+    student = models.OneToOneField(Student, on_delete=models.CASCADE)
+    card_id = models.CharField(max_length=20)
+    expiration_date = models.DateField()
+    creation_date = models.DateField(auto_now_add=True)
+    photo = models.ImageField(upload_to="student_photos")
+
+
+
 class Account(AbstractUser):
     email = models.EmailField("email address", max_length=255, blank=False, null=False, unique=True)
 
